66c4ffbb8d4b79d48baefa5350b837bfba16b721
yukihiro hara <yukihr@gmail.com>
2014-11-23 19:22:10 +0900

    create Article scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'articles#index'
   resources :articles
 
+  root to: 'articles#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
