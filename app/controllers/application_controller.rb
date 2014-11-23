3acfee82e8be77ae068d010d7fa9946128c4549c
yukihiro hara <yukihr@gmail.com>
2014-11-23 20:20:09 +0900

    sidebar tags using filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_sidebar_tags
+
+  private
+
+  def set_sidebar_tags
+    @sidebar_tags = Tag.all
+  end
 end
